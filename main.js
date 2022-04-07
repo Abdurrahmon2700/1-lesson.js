2c4c430eee48324ac71565772b60a68c6fc58606
Abdurrahmon2700 <sabrin1998@inbox.ru>
2022-04-07 12:04:52 +0500

    alert, promt

---
+++ main.js
@@ -0,0 +1,12 @@
+alert ('Xush kelibsiz');
+let namOfuser = prompt('Input name');
+let fullNameOfuser = prompt('Full name');
+let ageOfuser = prompt('Age');
+let jobOfuser = prompt('Job');
+alert ("Congratulations");
+
+console.log(`
+${namOfuser}
+${fullNameOfuser}
+${ageOfuser}
+${jobOfuser}`);
